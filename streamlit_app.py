cfe1c4afae66a6de85b91dd36f7dc7a6c69866d8
rserranom <rodrigoserranomillan@gmail.com>
2023-10-30 16:11:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner')
-streamlit.header('Breakfast menu')
-streamlit.text('Omega 3')
-streamlit.text('Kale, Spinach & Smoothie')
-streamlit.text('Hard boiled free-range egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado toast')
